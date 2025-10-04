f955fb6774396e05bcaf51d548d7636cb396f2fc
Daniel <daniel.losenfeld@rwth-aachen.de>
2025-10-04 12:54:24 +0200

    fixed typo

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,12 +14,12 @@ fn main() {
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failedeto read line");
+            .expect("Failede to read line");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        }
+        };
 
         println!("You guessed: {guess}");
 
