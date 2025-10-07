86f0a5b8cfbc090fc740350ec5278a1e8284d5ac
Daniel <daniel.losenfeld@rwth-aachen.de>
2025-10-07 20:12:51 +0200

    Chapter 4: Ownership

---
+++ 04_ownership/src/main.rs
@@ -0,0 +1,47 @@
+/*
+* Rules of ownership
+* - Each value in Rust has an owner
+* - There can only be one owner at a time 
+* - When the owner goes out of scope, the value will be dropped
+*/
+
+fn main() {
+    let mut s1 = String::from("hello");  // create String from string literal
+    
+    s1.push_str(", world!");             // String type can be mutated
+
+    // Move s1 into s2. This invalidates s1. (shallow copy and invalidation)
+    let mut s2 = s1;
+
+    // The clone() method allows for deep copying (expensive)
+    let s1 = s2.clone();
+
+    // New assignments will free previously owned memory via drop()
+    s2 = String::from("ahoy");
+    
+    // Copy trait annotates types stored on the stack
+    let x = 1;
+    let y = x;
+    println!("{x} = {y}");
+
+    /* 
+    * References
+    */
+
+    // Using a reference, we can access s1 without taking ownership
+    let _len =  calculate_length(&s1);
+    // Mutable references (&mut foo) can have no other references to that value!
+    println!("{s1} and {s2}!");
+
+    let reference_to_nothing = dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
